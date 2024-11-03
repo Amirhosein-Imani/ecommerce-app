d4809f68350559cddf556f1bedae9c16bda5f278
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2024-11-03 14:08:12 +0330

    start ecommerce app--commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Login from './pages/Login'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import About from './pages/About'
+import Product from './pages/Product'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vh] md:px-[7vh] lg:px-[9vh]'>
+
+        <Routes>
+
+          <Route path='/' element = {<Home/>} />
+          <Route path='/collection' element = {<Collection/>} />
+          <Route path='/orders' element = {<Orders/>} />
+          <Route path='/place-order' element = {<PlaceOrder/>} />
+          <Route path='/login' element = {<Login/>} />
+          <Route path='/contact' element = {<Contact/>} />
+          <Route path='/cart' element = {<Cart/>} />
+          <Route path='/about' element = {<About/>} />
+          <Route path='/product/:productId' element = {<Product/>} />
+
+        </Routes>
+
+    </div>
+  )
+}
+
+export default App
