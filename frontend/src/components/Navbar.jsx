05de2433297dc9e37b37358a3dc2ed68f746f9e3
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2024-11-06 16:55:10 +0330

    style & make navbar responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import {assets} from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
+import { Link , NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const [visible , setVisible] = useState(false)
+
   return (
 
     <div className='flex items-center justify-between py-5 font-medium'>
@@ -43,13 +46,31 @@ const Navbar = () => {
 
               <div className='group-hover:block hidden drop-down-menu absolute right-0 pt-4'>
 
-                <div className=''>
+                <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                        <p className='cursor-pointer hover:text-black'>My Profile</p>
+                        <p className='cursor-pointer hover:text-black'>Orders</p>
+                        <p className='cursor-pointer hover:text-black'>Logout</p>
 
                 </div>
 
               </div>
 
           </div>
+
+          <Link to='/cart' className='relative'>
+              
+              <img src={assets.cart_icon} alt="Cart Icon" className='w-5 min-w-5' />
+              <p className='absolute w-4 right-[-5px] bottom-[-5px] text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>0</p>
+
+          </Link>
+
+          <img src={assets.menu_icon} alt="Menu Icon" className='w-5 cursor-pointer sm:hidden' onClick={() => setVisible(true)}/>
+
+        </div>
+
+        {/* sidebar menu for small screens */}
+
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
 
         </div>
 
