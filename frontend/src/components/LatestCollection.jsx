aed67fa7c40816ed912d88535712115659eb4674
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2024-11-10 20:12:01 +0330

    add product item component

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
+import ProductItem from './ProductItem'
 
 const LatestCollection = () => {
 
@@ -17,7 +18,19 @@ const LatestCollection = () => {
         <div className='text-center py-8 text-3xl'>
 
             <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
-            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae quo commodi minima animi adipisci harum quaerat ex quos.</p>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 font-medium'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae quo commodi minima animi adipisci harum quaerat ex quos.</p>
+
+            {/* rendering products */}
+
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 pt-10'>
+
+              {
+                latestProducts.map((item , index) => (
+                  <ProductItem key={index} id={item.id} image={item.image} name={item.name} price={item.price}/>
+                ))
+              }
+
+            </div>
 
         </div>
   
