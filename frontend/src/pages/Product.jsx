07517d98dd3443e526aecb43338dd2028ee8a1d1
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2024-11-28 20:51:05 +0330

    finish product info section in product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -9,6 +9,7 @@ const Product = () => {
   const { products , currency} = useContext(ShopContext);
   const [productData , setProductData] = useState(false);
   const [image , setImage] = useState('');
+  const [size , setSize] = useState('');
 
   const fetchProductData = async () => {
 
@@ -91,10 +92,22 @@ const Product = () => {
               <div className='flex gap-2'>
                 {
               
-                  productData.sizes.map((item , index) => (<button key={index} className={`border py-2 px-4 bg-gray-100`}>{item}</button>))
+                  productData.sizes.map((item , index) => (<button onClick={() => setSize(item)} key={index} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`}>{item}</button>))
                 
                 }
               </div>
+
+            </div>
+
+            <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+
+            <hr className='mt-8 sm:w-4/5' />
+
+            <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+
+                <p>100% original product.</p>
+                <p>Cash on delivery is available on this product.</p>
+                <p>Easy return & exchange policy within 7 days.</p>
 
             </div>
 
