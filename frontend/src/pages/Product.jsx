67a612c4ebaaa299d72082ac952985ad3f35f282
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2024-12-04 19:37:09 +0330

    add display related products section in product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
@@ -135,7 +136,9 @@ const Product = () => {
 
       </div>
 
+      {/* ---------- display related products ---------- */}
 
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
 
     </div>
 
