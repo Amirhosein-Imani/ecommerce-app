95a44f770fa86b52a175f941487e2f9a6bdeb0f7
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2024-12-18 12:10:13 +0330

    Add proceed to checkout button in cart page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,7 +1,7 @@
 import { createContext , useState } from "react"
 import { products } from '../assets/assets'
 import { toast } from "react-toastify";
-
+import { useNavigate } from 'react-router-dom'
 
 export const ShopContext = createContext()
 
@@ -12,6 +12,7 @@ const ShopContextProvider = (props) => {
     const [search , setSearch] = useState('');
     const [showSearch , setShowSearch] = useState(false);
     const [cartItems , setCartItems] = useState({});
+    const navigate = useNavigate();
 
     const addToCart = async (itemId , size) => {
 
@@ -121,7 +122,7 @@ const ShopContextProvider = (props) => {
 
     const value = {
 
-        products , currency , delivery_fee , search , setSearch , showSearch , setShowSearch , cartItems , addToCart , getCartCount , updateQuantity , getCartAmount
+        products , currency , delivery_fee , search , setSearch , showSearch , setShowSearch , cartItems , addToCart , getCartCount , updateQuantity , getCartAmount , navigate
 
     }
 
