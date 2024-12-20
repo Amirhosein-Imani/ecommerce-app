e021994f784e840d6d5dc492e34c2854232e3003
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2024-12-20 15:49:40 +0330

    Add right side & payment method sections in place order page

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
+import CartTotal from '../components/CartTotal'
+import { assets } from '../assets/assets'
 
 const PlaceOrder = () => {
 
@@ -43,6 +45,54 @@ const PlaceOrder = () => {
         </div>
 
         <input type="number" placeholder='Phone' className='border border-gray-300 rounded py-1.5 px-3.5 w-full'/>
+
+      </div>
+
+      {/* ---------- right side ---------- */}
+
+      <div className='mt-8'>
+
+        <div className='mt-8 min-w-80'>
+
+          <CartTotal />
+
+        </div>
+
+        <div className='mt-12'>
+
+          <Title text1={'PAYMENT'} text2={'METHOD'}/>
+          
+          {/* ---------- payment method section ---------- */}
+
+          <div className='flex gap-3 flex-col lg:flex-row'>
+
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+
+              <p className={`min-w-3.5 h-3.5 border rounded-full `}></p>
+
+              <img src={assets.stripe_logo} alt="Stripe Logo" className='h-5 mx-4' />
+
+            </div>
+
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+
+              <p className={`min-w-3.5 h-3.5 border rounded-full `}></p>
+
+              <img src={assets.razorpay_logo} alt="Razorpay Logo" className='h-5 mx-4' />
+
+            </div>
+
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+
+              <p className={`min-w-3.5 h-3.5 border rounded-full `}></p>
+
+              <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
+
+            </div>
+
+          </div>
+  
+        </div>
 
       </div>
 
